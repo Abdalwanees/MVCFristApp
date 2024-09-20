efe437de6a144b31f76fab1c54e303d1ea2d99a4
Abdalwanees <gabdo7682@gmail.com>
2024-09-21 00:33:18 +0300

    Added Identity package and inherited from IdentityDbContext to create Identity tables

--- MVCFristApp.DAL/Data/AppDBContext.cs
+++ MVCFristApp.DAL/Data/AppDBContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MVCFristApp.DAL.Models;
 using System;
 using System.Collections.Generic;
@@ -8,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace MVCFristApp.DAL.Data
 {
-    public class AppDbContext :DbContext
+    public class AppDbContext :IdentityDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
         {
@@ -23,9 +25,20 @@ namespace MVCFristApp.DAL.Data
             modelBuilder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
 
             base.OnModelCreating(modelBuilder);
+            // Customize table names for Identity models
+            modelBuilder.Entity<IdentityUser>().ToTable("Users"); // جدول المستخدمين
+            modelBuilder.Entity<IdentityRole>().ToTable("Roles"); // جدول الأدوار
+            modelBuilder.Entity<IdentityUserRole<string>>().ToTable("User_Roles"); // جدول الربط بين المستخدمين والأدوار
+            modelBuilder.Entity<IdentityUserClaim<string>>().ToTable("User_Claims"); // جدول مطالبات المستخدم
+            modelBuilder.Entity<IdentityUserLogin<string>>().ToTable("User_Logins"); // جدول تسجيل دخول المستخدم
+            modelBuilder.Entity<IdentityRoleClaim<string>>().ToTable("Role_Claims"); // جدول مطالبات الأدوار
+            modelBuilder.Entity<IdentityUserToken<string>>().ToTable("User_Tokens"); // جدول الرموز (Tokens) الخاصة بالمستخدمين
         }
         public DbSet<Department> Departments { get; set; }  
         public DbSet<Employee> Employees { get; set; }
+         
+        //public IdentityUser Users { get; set; }
+        //public IdentityRole Roles { get; set; }
 
     }
 }
